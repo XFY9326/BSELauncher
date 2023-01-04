05bd1b8aa3ada08189f7630e7175ece9063de23f
XFY9326 <xfy9326@qq.com>
2023-01-04 19:23:25 +0000

    Update code

--- BSELauncher/BSELauncher.py
+++ BSELauncher/BSELauncher.py
@@ -94,41 +94,19 @@ class BSEMarketTask:
         self._prepare_output_dir(self.output_dir)
         if seed is not None:
             random.seed(seed)
-        with open(os.path.join(self.output_dir, f"{self.session_id}_avg_balance.csv"), "w") as f:
+        dump_file_path = os.path.join(self.output_dir, f"{self.session_id}_avg_balance.csv")
+        with open(dump_file_path, mode="w", encoding="utf-8") as f:
             for i in range(n):
                 self._launch(market_session_func, i, market_params, f)
 
     def _launch_in_parallel(self, market_session_func: callable, index: int, spec_dict: dict):
-        with open(os.path.join(self.output_dir, f"{self.session_id}_S{index}_avg_balance.csv"), "w") as f:
+        dump_file_dir = os.path.join(self.output_dir, f"{self.session_id}_S{index}_avg_balance.csv")
+        with open(dump_file_dir, mode="w", encoding="utf-8") as f:
             self._launch(market_session_func, index, spec_dict, f)
-
-    def _combine_avg_balance(self):
-        f_start = f"{self.session_id}_S"
-        f_end = "_avg_balance.csv"
-        avg_balance_files = [
-            (f, int(f[len(f_start):-len(f_end)]))
-            for f in os.listdir(self.output_dir)
-            if f.startswith(f_start) and f.endswith(f_end)
-        ]
-        sorted(avg_balance_files, key=lambda x: x[1])
-        with open(os.path.join(self.output_dir, f"{self.session_id}_avg_balance.csv"), "wb") as f_in:
-            for f_name, _ in avg_balance_files:
-                with open(os.path.join(self.output_dir, f_name), "rb") as f_out:
-                    f_in.write(f_out.read())
 
     # Running in parallel can speed things up, but using the specified seed results in exactly the same output
     # So here you can't set the random seed but use the default
     def launch_in_pool(self, market_session_func: callable, n: int, pool: Pool, complete_callback: Optional[callable] = None):
-        task_counter = 0
-
-        def combine_outputs(_):
-            nonlocal task_counter
-            task_counter += 1
-            if task_counter == n:
-                self._combine_avg_balance()
-            if complete_callback is not None:
-                complete_callback()
-
         if n <= 0:
             raise ValueError("n <= 0")
         market_params = self.spec.build()
@@ -137,14 +115,36 @@ class BSEMarketTask:
             pool.apply_async(
                 self._launch_in_parallel,
                 args=(market_session_func, i, market_params,),
-                callback=combine_outputs,
+                callback=lambda _: complete_callback(),
                 error_callback=_raise_error
             )
 
 
+def combine_avg_balance_csv_files(output_dir: str, session_id: str):
+    f_start = f"{session_id}_S"
+    f_end = "_avg_balance.csv"
+    avg_balance_files = [
+        (f, int(f[len(f_start):-len(f_end)]))
+        for f in os.listdir(output_dir)
+        if f.startswith(f_start) and f.endswith(f_end)
+    ]
+    avg_balance_files = sorted(avg_balance_files, key=lambda x: x[1])
+    dump_file_path = os.path.join(output_dir, f"{session_id}_avg_balance.csv")
+    with open(dump_file_path, mode="wb") as f_in:
+        for f_name, _ in avg_balance_files:
+            with open(os.path.join(output_dir, f_name), mode="rb") as f_out:
+                f_in.write(f_out.read())
+
+
 # Create a process for each task and run it in parallel
 # Note: Running multiple processes does not produce any output on the console
-def launch_market_tasks(market_session_func: callable, *tasks: BSEMarketTask, n: int = 1, seed: Optional[int] = None, workers: Optional[int] = None):
+def launch_market_tasks(
+        market_session_func: callable,
+        *tasks: BSEMarketTask,
+        n: int = 1,
+        seed: Optional[int] = None,
+        workers: Optional[int] = None
+):
     if n < 1:
         raise ValueError
     tasks_size = len(tasks)
@@ -176,7 +176,13 @@ def launch_market_tasks(market_session_func: callable, *tasks: BSEMarketTask, n:
 # Since the random number seed cannot be customized, the result cannot be completely reproduced
 # Faster than 'launch_market_tasks' only when the amount of tasks is small but the amount of sessions is large
 # Note: Running multiple processes does not produce any output on the console
-def launch_market_tasks_in_parallel(market_session_func: callable, *tasks: BSEMarketTask, n: int = 1, workers: Optional[int] = None):
+def launch_market_tasks_in_parallel(
+        market_session_func: callable,
+        *tasks: BSEMarketTask,
+        n: int = 1,
+        combine_avg_balances: bool = True,
+        workers: Optional[int] = None
+):
     if n < 1:
         raise ValueError
     if n == 1:
@@ -189,9 +195,12 @@ def launch_market_tasks_in_parallel(market_session_func: callable, *tasks: BSEMa
             with tqdm(total=tasks_size) as pbar:
                 p = Pool(processes=workers)
                 for task in tasks:
-                    task.launch_in_pool(market_session_func, n, p, lambda: pbar.update())
+                    task.launch_in_pool(market_session_func, n, p, pbar.update)
                 p.close()
                 p.join()
+                if combine_avg_balances:
+                    for task in tasks:
+                        combine_avg_balance_csv_files(task.output_dir, task.session_id)
         except KeyboardInterrupt:
             pass
         finally:
